85b53c312212338928270954d2d6187df4f1a566
DADAjiONsteroids <146656539+sarthak0417@users.noreply.github.com>
2026-07-06 13:12:10 +0530

    Time: 0 ms (100%), Space: 8.5 MB (7.31%) - LeetHub

---
+++ 1614-maximum-nesting-depth-of-the-parentheses/1614-maximum-nesting-depth-of-the-parentheses.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    int maxDepth(string s) {
+        stack<char> st;
+        int ans=0;
+
+        for (char ch:s){
+            if(ch=='('){
+                st.push(ch);
+                ans = max(ans,(int)st.size());
+            } else if (ch==')'){
+                st.pop();
+            }
+        }
+        return ans;
+    }
+};
+
+/*
+class Solution {
+public:
+    int maxDepth(string s) {
+        int currentDepth = 0;
+        int maxDepth=0;
+
+        for (char ch:s){
+            if(ch=='('){
+                currentDepth++;
+                maxDepth=max(maxDepth,currentDepth);
+            }else if(ch==')'){
+                currentDepth--;
+            }
+        }
+        return maxDepth;
+    }
+};
+*/
