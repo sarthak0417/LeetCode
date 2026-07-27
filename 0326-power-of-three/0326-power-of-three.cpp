fe5b017aa3ac7820125b7f522d820b3fb946a56d
DADAjiONsteroids <146656539+sarthak0417@users.noreply.github.com>
2026-07-27 19:46:16 +0530

    Time: 3 ms (59.26%), Space: 8.7 MB (98.07%) - LeetHub

---
+++ 0326-power-of-three/0326-power-of-three.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    bool isPowerOfThree(int n) {
+        if(n<=0) return false;
+        while(n%3==0)
+            n=n/3;
+        return n==1;
+    }
+};
+
+/*
+class Solution {
+public:
+    bool isPowerOfThree(int n) {
+        if(n<=0) return false;
+        
+        while(n>=3){
+            if(n%3 != 0) return false;
+            n=n/3;
+        }
+        return n==1;
+    }
+};
+*/
