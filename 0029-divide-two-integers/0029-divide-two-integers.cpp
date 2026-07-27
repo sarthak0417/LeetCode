3549f95069e573f39b14e4477448a282fa0ab168
DADAjiONsteroids <146656539+sarthak0417@users.noreply.github.com>
2026-07-27 20:49:06 +0530

    Time: 0 ms (100%), Space: 8.7 MB (39.81%) - LeetHub

---
+++ 0029-divide-two-integers/0029-divide-two-integers.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    int divide(int dividend, int divisor) {
+        if (dividend == divisor)
+            return 1;
+
+        bool sign = true;
+
+        if (dividend >= 0 && divisor < 0)
+            sign = false;
+        else if (dividend <= 0 && divisor > 0)
+            sign = false;
+
+        long long n = llabs((long long)dividend);
+        long long d = llabs((long long)divisor);
+
+        long long quotient = 0;
+
+        while (n >= d) {
+            int cnt = 0;
+            while (n >= (d << (cnt + 1))) {
+                cnt += 1;
+            }
+            quotient += 1 << cnt;
+            n -= (d << cnt);
+        }
+        if (quotient == (1 << 31) && sign) {
+            return INT_MAX;
+        }
+        if (quotient == (1 << 31) && !sign) {
+            return INT_MIN;
+        }
+        return sign ? quotient : -quotient;
+    }
+};
