2bc8075cad34033a4d59155eba7e91fde3631d9f
DADAjiONsteroids <146656539+sarthak0417@users.noreply.github.com>
2026-07-29 22:52:21 +0530

    Time: 0 ms (100%), Space: 8.9 MB (55.16%) - LeetHub

---
+++ 1021-remove-outermost-parentheses/1021-remove-outermost-parentheses.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    string removeOuterParentheses(string s) {
+        string ans;
+        int count = 0;
+
+        for (char c : s) {
+            if (c == '(') {
+                count++;
+
+                if (count > 1)
+                    ans += c;
+            } else {
+                count--;
+
+                if (count > 0)
+                    ans += c;
+            }
+        }
+
+        return ans;
+    }
+};
